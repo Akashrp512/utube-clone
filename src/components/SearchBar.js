e22c240971d314c8f08dad12b9afdc0d2fd5fda5
Akash RP <akashrp512@gmail.com>
2022-11-10 13:11:16 +0530

    axios added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
-const SearchBar = () => {
+class SearchBar extends React.Component {
+
+   state ={term: ''};
+   onInputChange = (event) =>{
+       this.setState ({term: event.target.value});
+   };
+
+   onFormSubmit = event =>{
+    event.preventDefault();
+
+    //Todo :make sure we call callback from parent component
+   };
+
+    render(){
   return (
-    <div>
-      Search Bar
+    <div className="search-bar ui segment">
+      <form className="ui form" onSubmit={this.onFormSubmit}>
+      <div className="field">
+        <label>Video Search</label>
+         <input type='text'value={this.state.term} onChange={this.onInputChange} />
+      </div>
+      </form>
     </div>
-  )
+  );
+}
 }
 
 export default SearchBar
