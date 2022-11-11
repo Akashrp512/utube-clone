162455f297bae47469844fcd0b9105bca7c6f2df
Akash RP <akashrp512@gmail.com>
2022-11-11 17:38:52 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ import youtube from './apis/youtube';
 class App extends React.Component {
       
   state ={videos: [], selectedVideo: null };
+
+  componentDidMount(){
+   this.onTermSubmit('Kannada latest Songs')
+  }
+
+
     onTermSubmit =async (term) =>{
      const response =  await youtube.get('/search',{
           params: {
@@ -18,19 +24,27 @@ class App extends React.Component {
         });
 
         
-        this.setState({videos : response.data.items })
+        this.setState({videos : response.data.items , selectedVideo: response.data.items[0]})
     };
      
     onVideoSelect =(video) => {
        this.setState({selectedVideo: video});
     };
-    
+
   render() {
   return (
     <div className="App ui container">
       <SearchBar  onFormSubmit={this.onTermSubmit}/>
+      <div className='ui grid'>
+      <div className='ui row'>
+      <div className='eleven wide column'>
       <VideoDetails video={this.state.selectedVideo}/>
+      </div>
+      <div className='five wide column'>
       <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+      </div>
+      </div>
+      </div>
     </div>
   );
 }
