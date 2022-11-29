4750b2436202b96846e5e828eeefd631ce12cf90
Akash RP <akashrp512@gmail.com>
2022-11-29 09:54:14 +0530

    Refactored primitive hooks to custom hooks

--- src/App.js
+++ src/App.js
@@ -4,33 +4,23 @@ import React, {useState, useEffect} from 'react';
 import SearchBar from './components/SearchBar';
 import VideoDetails from './components/VideoDetails';
 import VideoList from './components/VideoList';
-import youtube from './apis/youtube';
+import useVideos from './hooks/useVideos';
 
 const App =()=>{
-  const [videos, setVideos] = useState([]);
+ 
   const [selectedVideo, setSelectedVideo] = useState(null);
+  
+ const [videos, search] = useVideos('Latest kannada songs');
+    
+ useEffect(()=>{
+     setSelectedVideo(videos[0]);
+ },[videos]);
+ 
 
-
-  useEffect(()=>{
-    onTermSubmit('Kannada latest Songs');
-  },[]);
-
- const onTermSubmit =async (term) =>{
-    const response =  await youtube.get('/search',{
-         params: {
-           q: term
-         }
-       });
-       
-       setVideos(response.data.items);
-       setSelectedVideo(response.data.items[0]);
-       };
-
-
-
+ 
  return (
   <div className="App ui container">
-    <SearchBar  onFormSubmit={onTermSubmit}/>
+    <SearchBar  onFormSubmit={search}/>
     <div className='ui grid'>
     <div className='ui row'>
     <div className='eleven wide column'>
