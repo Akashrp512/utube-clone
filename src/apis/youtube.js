60ca6b06e64aabb4b9a4587f65dcb1c42bf4439c
Akash RP <Akashrp512@gmail.com>
2023-08-19 09:32:46 +0530

    Update youtube.js
    Removed KEY use youtube key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBltjIO_UE37bG7f-s3TF8pUCabqupRwPQ';
-
 export default axios.create({
    baseURL: 'https://www.googleapis.com/youtube/v3',
    params: {
